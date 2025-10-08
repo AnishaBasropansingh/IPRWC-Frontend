a005f715d4370c700aea1ca7d10b4b6ead5f4c9e
AzureAD\AnishaBasropansingh <abasropansingh@swis.nl>
2025-10-08 16:02:15 +0200

    Feat: added nav and basic page routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {Home} from './page/home/home';
+import {Product} from './page/product/product';
+import {About} from './page/about/about';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'product', component: Product},
+  { path: 'about', component: About },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
