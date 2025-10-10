b984579e408496b4d5735361bda16aa18cbaf2d3
AzureAD\AnishaBasropansingh <abasropansingh@swis.nl>
2025-10-10 11:46:39 +0200

    feat: added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Home} from './page/home/home';
 import {Navbar} from './component/shared/navbar/navbar';
+import {Footer} from './component/shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
