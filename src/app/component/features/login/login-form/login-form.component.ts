1af3d548edb3bad2ef2cec4b5d605ebab1a2e30b
AzureAD\AnishaBasropansingh <abasropansingh@swis.nl>
2025-10-14 16:52:38 +0200

    feat: added the login form + styled it

--- src/app/component/features/login/login-form/login-form.component.ts
+++ src/app/component/features/login/login-form/login-form.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
 
+  onSubmit() {
+    if (this.email && this.password) {
+      console.log('Inloggen met:', this.email, this.password);
+      // AUTHSERVICE call toevoegen
+    } else {
+      this.errorMessage = 'Vul alle velden in.';
+    }
+  }
 }
