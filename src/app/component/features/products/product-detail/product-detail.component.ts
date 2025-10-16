fc1e033bc4a2cfb314c1a5ed932b7f56d874239f
AzureAD\AnishaBasropansingh <abasropansingh@swis.nl>
2025-10-16 13:26:36 +0200

    Feat: finished the product detail page and disabled cart button if not logged in

--- src/app/component/features/products/product-detail/product-detail.component.ts
+++ src/app/component/features/products/product-detail/product-detail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {Observable} from 'rxjs';
-import {Product} from '../../../../model/product';
-import {ProductService} from '../../../../service/product/product.service';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil, map, catchError } from 'rxjs/operators';
+import { Product } from '../../../../model/product';
+import { ProductService } from '../../../../service/product/product.service';
+import {AuthService} from '../../../../service/auth/auth.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,16 +14,34 @@ import {ProductService} from '../../../../service/product/product.service';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
   public product$!: Observable<Product>;
+  private _destroy$ = new Subject<void>();
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    protected authService: AuthService
   ) {}
 
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.product$ = this.productService.getProductById(id); // Observable<Product>
+
+    this.product$ = this.productService.getProductById(id).pipe(
+      takeUntil(this._destroy$),
+      map((res: Product) => {
+
+        return { ...res, price: res.price / 100 };
+      }),
+      catchError(err => {
+        console.error('Fout bij ophalen product:', err);
+        throw err;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 }
